ef36e72840ed60344fa90ef00a75a49ef79769db
Krzysztof Grochocki <beherit@users.noreply.github.com>
2013-08-11 15:16:05 +0200

    Dodanie licencji GNU GPL v3 i pliku readme oraz ma?a zmiana opisu wtyczki

--- SideSlideExceptionsFrm.h
+++ SideSlideExceptionsFrm.h
@@ -1,4 +1,25 @@
 //---------------------------------------------------------------------------
+// Copyright (C) 2010-2013 Krzysztof Grochocki
+//
+// This file is part of TabKit
+//
+// TabKit is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// TabKit is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with GNU Radio; see the file COPYING. If not, write to
+// the Free Software Foundation, Inc., 51 Franklin Street,
+// Boston, MA 02110-1301, USA.
+//---------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------
 #ifndef SideSlideExceptionsFrmH
 #define SideSlideExceptionsFrmH
 #define WM_ALPHAWINDOWS (WM_USER + 666)
