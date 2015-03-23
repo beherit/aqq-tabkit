7a6065837d9d04651b04c6b14e9830d845abc27a
Krzysztof Grochocki <beherit@users.noreply.github.com>
2015-03-23 12:47:55 +0100

    Aktualizacja kr?tkiego opisu.

--- TabKit.cpp
+++ TabKit.cpp
@@ -12591,7 +12591,7 @@ extern "C" PPluginInfo __declspec(dllexport) __stdcall AQQPluginInfo(DWORD AQQVe
 	PluginInfo.cbSize = sizeof(TPluginInfo);
 	PluginInfo.ShortName = L"TabKit";
 	PluginInfo.Version = PLUGIN_MAKE_VERSION(1,10,0,2);
-	PluginInfo.Description = L"Wtyczka oferuje masê funkcjonalnoœci usprawniaj¹cych korzystanie z komunikatora np. zapamiêtywanie zamkniêtych zak³adek, inteligentne prze³¹czanie, zapamiêtywanie sesji.";
+	PluginInfo.Description = L"Oferuje masê funkcjonalnoœci usprawniaj¹cych korzystanie z komunikatora np. zapamiêtywanie zamkniêtych zak³adek, inteligentne prze³¹czanie, zapamiêtywanie sesji.";
 	PluginInfo.Author = L"Krzysztof Grochocki";
 	PluginInfo.AuthorMail = L"kontakt@beherit.pl";
 	PluginInfo.Copyright = L"Krzysztof Grochocki";
